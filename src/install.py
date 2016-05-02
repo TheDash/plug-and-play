b2a8386b8dc43078edac0317e3525297eff0ec21
TheDash <noobaca2@gmail.com>
2016-05-02 12:06:19 -0400

    added usage for usb and ethernet

--- src/install.py
+++ src/install.py
@@ -7,7 +7,32 @@
         - prompt for x,y,z positioning and the frame the offset is relative to (default: base_link)
         - add a launch file to spawn the node on boot with correct parameters. The launch file launches the ROS driver and the static transform publisher
 
+    Usage:
+
+        For ethernet devices:
+
+            rosrun plug-and-play install [module_name] usb [ip_addr] [x, y, z]
+
+        For USB devices:
+
+            rosrun plug-and-play install [module_name] usb [mac_address] [x, y, z]
+
 """
 
 
 import rospy
+import argparse
+
+
+def install(args):
+
+
+if __name__ == '__main__':
+    try:
+
+        args = sys.argv[1:]
+
+
+        install(sys.argv[1:])
+    except rospy.ROSInterruptException:
+        pass
