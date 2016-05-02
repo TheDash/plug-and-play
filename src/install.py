e7bf63b9c21d0312fb0b4f5fa07c8d7c4e9b4460
TheDash <noobaca2@gmail.com>
2016-05-02 12:28:49 -0400

    Added arguments to arg parser

--- src/install.py
+++ src/install.py
@@ -11,7 +11,7 @@
 
         For ethernet devices:
 
-            rosrun plug-and-play install [module_name] usb [ip_addr] [x, y, z]
+            rosrun plug-and-play install [module_name] eth [ip_addr] [x, y, z]
 
         For USB devices:
 
@@ -20,19 +20,33 @@
 """
 
 
-import rospy
+#import rospy
 import argparse
 
 
-def install(args):
+#def install(args):
+
+def add_eth_module(module_name, ip_addr, position):
+    print "Adding eth module " + module_name
+    return 0
+
+def add_usb_module(module_name, mac_address, position):
+    print "Adding usb module " + module_name
+    return 0
 
 
 if __name__ == '__main__':
-    try:
+    parser = argparse.ArgumentParser(description="Installs a module to your robot to be used for plug-and-play")
+    parser.add_argument("--module", dest="module", help="the name of the module. e.g velodynehdle32, kinect2, kinect1")
+    parser.add_argument("--position", dest="position", help="the position of the module relative to the frame name -f, e.g [10, 12, 13]")
+    parser.add_argument("--frame", dest="frame", help="the frame to which the static transform will be relative to for the position of the sensor")
+    parser.add_argument("--type", dest="type", help="the type of module to install, e.g eth for ethernet, usb for usb, bluetooth for bluetooth")
+    parser.add_argument("--ip_addr", dest="ip_addr", help="the ip address of the module if eth is the type")
+    parser.add_argument("--mac_addr", dest="mac_addr", help="the mac address of the device if using usb as the type")
 
-        args = sys.argv[1:]
-
-
-        install(sys.argv[1:])
-    except rospy.ROSInterruptException:
-        pass
+    args = parser.parse_args()
+    
+#    args = sys.argv[1:]
+#    install(sys.argv[1:])
+    #except rospy.ROSInterruptException:
+    #    pass
