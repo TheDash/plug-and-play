97f5ca389653a82f9e63ad7a9638f42577234b43
TheDash <noobaca2@gmail.com>
2016-05-02 12:44:38 -0400

    Added argmmodules

--- src/install.py
+++ src/install.py
@@ -43,9 +43,35 @@ if __name__ == '__main__':
     parser.add_argument("--type", dest="type", help="[REQUIRED] the type of module to install, e.g eth for ethernet, usb for usb, bluetooth for bluetooth")
     parser.add_argument("--ip_addr", dest="ip_addr", help="the ip address of the module if eth is the type")
     parser.add_argument("--mac_addr", dest="mac_addr", help="the mac address of the device if using usb as the type")
+    parser.add_argument("--list-available", dest="list", help="List all of the available modules to install, e.g to use as a module name")
+     
 
     args = parser.parse_args()
-    
+
+    if not args.module:
+        print "The argument --module is not set. Please pick a module to install"
+        return 0
+
+    if not args.position:
+        print "The argument --position is not set. Please set the position [x, y, z]"    
+        return 0
+   
+    if not args.frame:
+        print "The argument --frame is not set. Please pick a relative frame to set for the static transform"
+        return 0   
+
+    if not args.type:
+        print "The type of the module to install is not set. Please pick a type"
+        return 0
+
+    if args.type == "eth" && not args.ip_addr:
+        print "Ethernet module selected, but no IP address of ethernet device is known. Please find out the IP address of your device"
+        return 0
+
+     if args.type == "usb" && not args.mac-addr:
+        print "USB module selected, but no mac address of device is known. Please find out the mac address of your device"
+        return 0
+
 #    args = sys.argv[1:]
 #    install(sys.argv[1:])
     #except rospy.ROSInterruptException:
